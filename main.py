cdeb4bfa28c852f08816fabfb4a82dcc3dabcaa1
William Eddy <will_e1991@msn.com>
2025-02-02 17:40:05 -0700

    Added a black screenthat will close when user closes screen

---
+++ main.py
@@ -0,0 +1,23 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+# imports in the constants.py and the * imports the whole file
+from constants import *
+
+
+def main():
+    pygame.init
+    print("Starting asteroids!\nScreen width: 1280\nScreen height: 720")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.init()
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
